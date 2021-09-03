441ec3a8117ff9096d4d552b7dcb59b587bc1885
magicsk <magicsk@magicsk.eu>
2021-09-03 22:14:19 +0000

    fixed gif links

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ fetch('https://api.giphy.com/v1/gifs/random?api_key=1e37df9386bd4014b9b0cd305632
     .then(data => {
         let url = `url(${data.data.image_url})`;
         document.getElementById('leftImg').style.backgroundImage = url;
-        document.getElementById('fggif').src = data.data.url;
+        document.getElementById('fggif').href = data.data.url;
     });
 
 fetch('https://api.giphy.com/v1/gifs/random?api_key=1e37df9386bd4014b9b0cd305632e41c&rating=r&tag=javascript')
@@ -11,7 +11,7 @@ fetch('https://api.giphy.com/v1/gifs/random?api_key=1e37df9386bd4014b9b0cd305632
     .then(data => {
         let url = `url(${data.data.image_url})`;
         document.getElementsByTagName('body')[0].style.backgroundImage = url;
-        document.getElementById('bggif').src = data.data.url;
+        document.getElementById('bggif').href = data.data.url;
     });
 
 
