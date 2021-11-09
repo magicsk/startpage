4dbfb893789ed5999f0b9861270a94d714fa5d2d
magicsk <magicsk@magicsk.eu>
2021-11-09 16:28:48 +0100

    added options and optimalised layout for mobile

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+let links_container = [
+    'Hosted',
+    'Home',
+    'https://home.magicsk.eu/',
+    'Jellyfin',
+    'https://media.magicsk.eu/',
+    'Bitwarden',
+    'https://bitwarden.magicsk.eu/',
+    '',
+    '',
+    'Development',
+    'API',
+    'https://api.magicsk.eu/',
+    'localhost',
+    'http://localhost:3000/',
+    'VSCode',
+    'https://code.magicsk.eu/',
+    '',
+    '',
+    'Social',
+    'Instagram',
+    'https://www.instagram.com/rastislavbrna/',
+    'GitHub',
+    'https://github.com/magicsk/',
+    'Gists',
+    'https://gist.github.com/magicsk/',
+    'Reddit',
+    'https://www.reddit.com/user/magic_sk/',
+    'Projects',
+    'Lost Henn',
+    'https://magicsk.github.io/lost-henn/',
+    'MHD Virtual Table',
+    'https://github.com/magicsk/mhd_virtual_table/',
+    'Max Miedinger',
+    'https://maxmiedinger.magicsk.eu/',
+    '',
+    ''
+];
+const urlLines = [2, 4, 6, 8, 11, 13, 15, 17, 20, 22, 24, 26, 29, 31, 33, 35];
+let maxWidthSlider = document.getElementById('maxWidthRange');
+
 fetch('https://api.giphy.com/v1/gifs/random?api_key=1e37df9386bd4014b9b0cd305632e41c&rating=r&tag=pattern')
     .then(response => response.json())
     .then(data => {
@@ -15,7 +56,7 @@ fetch('https://api.giphy.com/v1/gifs/random?api_key=1e37df9386bd4014b9b0cd305632
     });
 
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     if (!window.AnimationEvent) {
         return;
     }
@@ -27,9 +68,9 @@ document.addEventListener('DOMContentLoaded', function () {
             continue;
         }
 
-        anchors[idx].addEventListener('click', function (event) {
+        anchors[idx].addEventListener('click', function(event) {
             var fader = document.getElementById('fader'), anchor = event.currentTarget;
-            var listener = function () {
+            var listener = function() {
                 window.location = anchor.href;
                 fader.removeEventListener('animationend', listener);
             };
@@ -40,7 +81,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-window.addEventListener('pageshow', function (event) {
+window.addEventListener('pageshow', function(event) {
     if (!event.persisted) {
         return;
     }
@@ -55,12 +96,12 @@ const lightModeToggle = document.querySelector('#light-mode-toggle');
 const enableLightMode = () => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightMode', 'enabled');
-}
+};
 
 const disableLightMode = () => {
     document.body.classList.remove('lightmode');
     localStorage.setItem('lightMode', null);
-}
+};
 
 if (lightMode === 'enabled') {
     enableLightMode();
@@ -78,13 +119,13 @@ lightModeToggle.addEventListener('click', () => {
 });
 
 function doDate() {
-    var str = "";
-    var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    var str = '';
+    var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
     var clock = new Date();
     var now = new Date();
 
-    str += months[clock.getMonth()] + " " + now.getDate() + ", " + clock.getFullYear() + " | " + ("0" + now.getHours()).slice(-2) + ":" + ("0" + now.getMinutes()).slice(-2) + ":" + ("0" + now.getSeconds()).slice(-2);
-    document.getElementById("date").innerHTML = str;
+    str += months[clock.getMonth()] + ' ' + now.getDate() + ', ' + clock.getFullYear() + ' | ' + ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2) + ':' + ('0' + now.getSeconds()).slice(-2);
+    document.getElementById('date').innerHTML = str;
 }
 setInterval(doDate, 1000);
 doDate();
@@ -96,4 +137,87 @@ function fadeInPage() {
     var fader = document.getElementById('fader');
     fader.classList.add('fade-out');
 }
+
+function loadLocalStorage() {
+    let empty = localStorage.getItem('isEmpty');
+    if (empty != 'false') {
+        localStorage.setItem('isEmpty', false);
+        localStorage.setItem('title', 'magic_sk');
+        localStorage.setItem('links_container', JSON.stringify(links_container));
+        localStorage.setItem('max-width', 1200);
+        alert('Customize everything by clicking on Options in top left corner.');
+        loadLocalStorage();
+    } else {
+        let title = localStorage.getItem('title');
+        document.getElementById('pageTitle').innerText = title;
+        document.getElementById('titleTextInput').value = title;
+        
+        links_container = JSON.parse(localStorage.getItem('links_container'));
+        let inputs = document.getElementById('editModalForms').getElementsByTagName('input');
+        
+        for (let i = 2; i < inputs.length; i++) {
+            inputs[i].value = links_container[i - 2];
+        }
+        
+        let linksContainers = document.getElementById('links_container').getElementsByTagName('a');
+        let linksContainersHref = document.getElementById('links_container').getElementsByTagName('li');
+        let j = 0, k = 0;
+        
+        for (let i = 0; i < links_container.length; i++) {
+            if (urlLines.includes(i)) {
+                linksContainersHref[j++].lastChild.href = links_container[i];
+            } else {
+                linksContainers[k++].innerText = links_container[i];
+            }
+        }
+
+        let maxWidth = localStorage.getItem('max-width');
+        maxWidth = parseInt(maxWidth);
+        let maxWidthText = document.getElementById('maxWidthRangeValue');
+        let mainWindow = document.getElementById('main_window');
+        maxWidthSlider.value = maxWidth;
+        maxWidthText.innerText = maxWidth;
+        mainWindow.style.maxWidth = `${maxWidth}px`;
+    }
+}
+
+loadLocalStorage();
 fadeInPage();
+
+//Options open/close
+document.getElementById('editButton').addEventListener('click', () => {
+    let modal = document.getElementById('editModal');
+    modal.style.visibility = 'visible';
+});
+
+document.getElementById('editModal').addEventListener('click', () => {
+    let modal = document.getElementById('editModal');
+    modal.style.visibility = 'hidden';
+});
+
+document.getElementById('editModalBody').addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+//Options submit button
+document.getElementById('editModalSubmitBtn').addEventListener('click', () => {
+    let inputs = document.getElementById('editModalForms').getElementsByTagName('input');
+    let title = inputs[0].value;
+    localStorage.setItem('title', title);
+
+    for (let i = 2; i < inputs.length; i++) {
+        links_container[i - 2] = inputs[i].value;
+    }
+
+    localStorage.setItem('links_container', JSON.stringify(links_container));
+    localStorage.setItem('max-width', maxWidthSlider.value);
+
+    loadLocalStorage();
+
+    let modal = document.getElementById('editModal');
+    modal.style.visibility = 'hidden';
+});
+
+maxWidthSlider.oninput = () => {
+    document.getElementById('maxWidthRangeValue').innerText = maxWidthSlider.value;
+};
